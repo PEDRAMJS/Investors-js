2e58adae373c4aab87ec26c785b498c9a161dbf2
PEDRAMJS <pedramjalaeyan@gmail.com>
2026-01-08 00:01:00 +0330

    MVP

--- src/index.js
+++ src/index.js
@@ -13,7 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://94.182.92.245', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
